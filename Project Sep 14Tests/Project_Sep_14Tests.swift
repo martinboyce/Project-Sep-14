b6e3922df3381b78c0fb6bc19b15925776727ad1
Martin Boyce <martinboyce@umass.edu>
2021-09-14 13:04:13 -0400

    Initial Commit

---
+++ Project Sep 14Tests/Project_Sep_14Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Project_Sep_14Tests.swift
+//  Project Sep 14Tests
+//
+//  Created by Martin Boyce on 9/14/21.
+//
+
+import XCTest
+@testable import Project_Sep_14
+
+class Project_Sep_14Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
